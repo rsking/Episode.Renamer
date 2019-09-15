bc2fc7efdb2297cc87fabadcc5df6a36cf6e00ea
Ross King <rsking@gmail.com>
2019-09-16 08:50:22 +1000

    Initial Commit

---
+++ Episode.Renamer/ExtensionMethods.cs
@@ -0,0 +1,86 @@
+﻿namespace Episode.Renamer
+{
+    internal static class ExtensionMethods
+    {
+        private static readonly TagLib.ReadOnlyByteVector StikAtom = "stik";
+
+        public static uint GetUInt32(this TagLib.Mpeg4.AppleTag appleTag, TagLib.ReadOnlyByteVector type)
+        {
+            foreach (var item in appleTag.DataBoxes(type))
+            {
+                if (item.Data.Count == 4)
+                {
+                    byte[] data;
+                    if (System.BitConverter.IsLittleEndian)
+                    {
+                        data = new byte[item.Data.Count];
+                        data[0] = item.Data.Data[3];
+                        data[1] = item.Data.Data[2];
+                        data[2] = item.Data.Data[1];
+                        data[3] = item.Data.Data[0];
+                    }
+                    else
+                    {
+                        data = item.Data.Data;
+                    }
+
+                    return System.BitConverter.ToUInt32(data);
+                }
+            }
+
+            return 0u;
+        }
+
+        public static bool IsMovie(this TagLib.Mpeg4.AppleTag appleTag)
+        {
+            foreach (var item in appleTag.DataBoxes(StikAtom))
+            {
+                if (item.Data.Count == 1)
+                {
+                    return item.Data.Data[0] == 9;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool IsTvShow(this TagLib.Mpeg4.AppleTag appleTag)
+        {
+            foreach (var item in appleTag.DataBoxes(StikAtom))
+            {
+                if (item.Data.Count == 1)
+                {
+                    return item.Data.Data[0] == 10;
+                }
+            }
+
+            return false;
+        }
+
+        public static string ReplaceAll(this string stringValue, char[] oldValues, char newValue)
+        {
+            foreach (var oldValue in oldValues)
+            {
+                stringValue = stringValue.Replace(oldValue, newValue);
+            }
+
+            return stringValue;
+        }
+
+#if !NETCOREAPP3_0
+        public static void MoveTo(this System.IO.FileInfo fileInfo, string destFileName, bool overwrite)
+        {
+            if (overwrite)
+            {
+                var tempFileName = System.IO.Path.GetTempFileName();
+                fileInfo.Replace(destFileName, null);
+                System.IO.File.Delete(tempFileName);
+            }
+            else
+            {
+                fileInfo.MoveTo(destFileName);
+            }
+        }
+#endif
+    }
+}
---
+++ Episode.Renamer/Program.cs
@@ -0,0 +1,131 @@
+﻿namespace Episode.Renamer
+{
+    using System.CommandLine;
+    using System.CommandLine.Invocation;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using Serilog;
+    using TagLib;
+
+    internal class Program
+    {
+        private static readonly ReadOnlyByteVector EpisodeNumber = "tves";
+        private static readonly ReadOnlyByteVector ShowName = "tvsh";
+        private static readonly ReadOnlyByteVector SeasonNumber = "tvsn";
+
+        private static Task<int> Main(string[] args)
+        {
+            var host = new Microsoft.Extensions.Hosting.HostBuilder().ConfigureServices((_, services) =>
+            {
+                var serilogLogger = new LoggerConfiguration()
+                    .WriteTo.ColoredConsole().MinimumLevel.Information()
+                    .CreateLogger();
+                services.AddLogging(c => c.AddSerilog(serilogLogger, true));
+            }).Build();
+
+            var root = new RootCommand();
+            root.AddArgument(new Argument<System.IO.DirectoryInfo>("source"));
+            root.AddArgument(new Argument<System.IO.DirectoryInfo>("destination"));
+            root.AddOption(new Option(new[] { "-m", "--move" }, "Moves the files") { Argument = new Argument<bool>("move") });
+            root.AddOption(new Option(new[] { "-n", "--dry-run" }, "Don’t actually move/copy any file(s). Instead, just show if they exist and would otherwise be moved/copied by the command."));
+
+            root.Handler = CommandHandler.Create((System.IO.DirectoryInfo source, System.IO.DirectoryInfo destination, bool move, bool dryRun) =>
+            {
+                // search for all files in the source directory
+                var programLogger = host.Services.GetRequiredService<ILogger<Program>>();
+
+                foreach (var file in source.EnumerateFiles("*.*", new System.IO.EnumerationOptions { RecurseSubdirectories = true, IgnoreInaccessible = true, AttributesToSkip = System.IO.FileAttributes.Hidden }))
+                {
+                    if (file.Length == 0)
+                    {
+                        continue;
+                    }
+
+                    File tagLibFile = default;
+                    System.IO.FileInfo path = default;
+
+                    try
+                    {
+                        tagLibFile = File.Create(file.FullName);                        
+
+                        if (tagLibFile.GetTag(TagTypes.Apple) is TagLib.Mpeg4.AppleTag appleTag)
+                        {
+                            if (appleTag.IsMovie())
+                            {
+                                var directory = System.IO.Path.Combine(destination.FullName, "Movies").ReplaceAll(System.IO.Path.GetInvalidPathChars(), '_');
+                                var fileName = $"{appleTag.Title} ({appleTag.Year}){file.Extension}".ReplaceAll(System.IO.Path.GetInvalidFileNameChars(), '_');
+                                path = new System.IO.FileInfo(System.IO.Path.Combine(directory, fileName));
+                            }
+                            else if (appleTag.IsTvShow())
+                            {
+                                var showName = string.Join("; ", appleTag.GetText(ShowName));
+                                var seasonNumber = appleTag.GetUInt32(SeasonNumber);
+                                var episodeNumber = appleTag.GetUInt32(EpisodeNumber);
+                                var episodeName = appleTag.Title;
+
+                                var directory = System.IO.Path.Combine(destination.FullName, "TV Shows", showName, $"Season {seasonNumber:00}").ReplaceAll(System.IO.Path.GetInvalidPathChars(), '_');
+                                var fileName = $"{showName} - s{seasonNumber:00}e{episodeNumber:00} - {episodeName}{file.Extension}".ReplaceAll(System.IO.Path.GetInvalidFileNameChars(), '_');
+
+                                path = new System.IO.FileInfo(System.IO.Path.Combine(directory, fileName));
+                            }
+                            else
+                            {
+                                programLogger.LogInformation("Failed to match {File} to either Movie or TV Show", file.Name);
+                            }
+                        }
+
+                        tagLibFile.Mode = File.AccessMode.Closed;
+                    }
+                    catch (UnsupportedFormatException)
+                    {
+                    }
+                    catch (CorruptFileException)
+                    {
+                    }
+                    finally
+                    {
+                        tagLibFile?.Dispose();
+                    }
+
+                    if (path != null)
+                    {
+                        if (path.Exists)
+                        {
+                            // see if this is the name size
+                            if (path.Length == file.Length)
+                            {
+                                programLogger.LogDebug("{Source} has the same length as {Destination}", file.FullName, path.FullName);
+                                continue;
+                            }
+                        }
+
+                        if (!dryRun)
+                        {
+                            path.Directory.Create();
+                        }
+
+                        if (move)
+                        {
+                            programLogger.LogInformation("Moving {Source} to {Destination}", file.FullName, path.FullName);
+                            if (!dryRun)
+                            {
+                                file.MoveTo(path.FullName, true);
+                            }
+                        }
+                        else
+                        {
+                            programLogger.LogInformation("Coping {Source} to {Destination}", file.FullName, path.FullName);
+                            if (!dryRun)
+                            {
+                                file.CopyTo(path.FullName, true);
+                            }
+                        }
+                    }
+                }
+            });
+
+            return root.InvokeAsync(args);
+        }
+    }
+}
